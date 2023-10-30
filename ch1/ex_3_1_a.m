888f68196c91beabcdabb5c60fdabeaadc486e9b
ninux <nino.ninux@gmail.com>
2023-10-30 22:19:05 +0100

    started to fix file encodings and formatting

--- ch1/ex_3_1_a.m
+++ ch1/ex_3_1_a.m
@@ -11,9 +11,9 @@ h         = 6.626e-34;      % [J*s]     Plank's constant
 hbar      = h/(2*pi);       % [J*s]     reduced Plank's constant
 m0        = 9.1e-31;        % [kg]      free space mas of an electron
 
-meff_Si   = 1.08;           % [?]		effective mass of Si
-meff_Ge   = 0.067;          % [?]		effective mass of Ge
-meff_GaAs = 0.55;           % [?]		effective mass of GaAs
+meff_Si   = 1.08;           % [?]       effective mass of Si
+meff_Ge   = 0.067;          % [?]       effective mass of Ge
+meff_GaAs = 0.55;           % [?]       effective mass of GaAs
 
 meff      = meff_Si;        % [?]       effective mass
 melec     = meff*m0;        % [?]       mass of an electron
@@ -31,7 +31,7 @@ DX        = del_x*1e9;      % [-]       cell size in nm
 XX        = (DX:DX:DX*NN);  % [-]       length in nm for plotting
 
 %% specification of the potential
-V         = zeros(1,NN);    % [J]		init potential vector
+V         = zeros(1,NN);    % [J]       init potential vector
 
 
 %% potential definitions
@@ -57,17 +57,17 @@ end
 
 %% initialize a sine wave in a gaussian envelope
 
-lambda	= 10;           % [nm]		pulse wavelength
-sigma   = 50;           % [nm]		pulse width
-nc      = 200;          % [-]		starting position
-prl     = zeros(1,NN);  % [?]		real part of the state variable
-pim     = zeros(1,NN);	% [?]		imaginary part of the state variable
-ptot    = 0.;           % [?]		state variable	
+lambda  = 10;           % [nm]      pulse wavelength
+sigma   = 50;           % [nm]      pulse width
+nc      = 200;          % [-]       starting position
+prl     = zeros(1,NN);  % [?]       real part of the state variable
+pim     = zeros(1,NN);  % [?]       imaginary part of the state variable
+ptot    = 0.;           % [?]       state variable    
 
 for n=2:NN-1
-	prl(n)	= exp(-1.*((n-nc)/sigma)^2) * cos(2*pi*(n-nc)/lambda);
-    pim(n)	= exp(-1.*((n-nc)/sigma)^2) * sin(2*pi*(n-nc)/lambda);
-    ptot	= ptot + prl(n)^2 + pim(n)^2;
+    prl(n)  = exp(-1.*((n-nc)/sigma)^2) * cos(2*pi*(n-nc)/lambda);
+    pim(n)  = exp(-1.*((n-nc)/sigma)^2) * sin(2*pi*(n-nc)/lambda);
+    ptot    = ptot + prl(n)^2 + pim(n)^2;
 end
 
 pnorm = sqrt(ptot);     % normalization constant
@@ -76,25 +76,25 @@ pnorm = sqrt(ptot);     % normalization constant
 ptot = 0.;
 
 for n=1:NN
-    prl(n)	= prl(n)/pnorm;	
+    prl(n)  = prl(n)/pnorm;
     pim(n)  = pim(n)/pnorm;
     ptot    = ptot + prl(n)^2 + pim(n)^2;
 end
 
 fprintf("normalization = %f\n", ptot);    % should have the value 1
 
-T     	= 0;
+T       = 0;
 n_step  = 1;
-count   = 0;    
+count   = 0;
 nos     = 400;  % number of timestepts between plots (equidistant)
 nop     = 3;    % number of plots to be made
 
 while nop > count
-	%n_step = input("How many time steps -->");
+    %n_step = input("How many time steps -->");
 
-	% this is the core FDTD program
+    % this is the core FDTD program
     for m=1:n_step
-    	T = T+1;
+        T = T+1;
     
         for n=2:NN-1
             prl(n) = prl(n) - ra*(pim(n-1) -2*pim(n) + pim(n+1)) ...
@@ -108,7 +108,7 @@ while nop > count
     end
     
     % calculate the expected values
-    PE = 0.;	% potential energy
+    PE = 0.;    % potential energy
     
     for n=1:NN
         psi(n) = prl(n) + i*pim(n);
@@ -117,8 +117,8 @@ while nop > count
 
     fprintf("normalization = %f\n", psi*psi');
 
-    PE = PE*J2eV;    	% potential energy
-    ke = 0. + j*0.;		
+    PE = PE*J2eV;        % potential energy
+    ke = 0. + j*0.;        
     
     for n=2:NN-1
         lap_p = psi(n+1) - 2*psi(n) + psi(n-1);
@@ -130,7 +130,7 @@ while nop > count
     % calculate the expected position
     x_avg = 0;
     for n=1:NN
-      x_avg += ((prl(n))^2 + (pim(n))^2)*(n*del_x);
+        x_avg += ((prl(n))^2 + (pim(n))^2)*(n*del_x);
     endfor
 
     % plot results
@@ -154,9 +154,9 @@ while nop > count
     
     set(gca, "fontsize", 10);
     if (count == 0)
-      title("Exercise 3-1 a - 1D FDTD Simulation");
+        title("Exercise 3-1 a - 1D FDTD Simulation");
     else
-      title(" ");
+        title(" ");
     endif
     
     grid on;
